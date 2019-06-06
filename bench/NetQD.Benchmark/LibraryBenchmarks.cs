1c086cc78a580685fcb2f734b3b8ab2904901398
Radek Zikmund <r.zikmund.rz@gmail.com>
2019-06-06 22:33:55 +0200

    Add benchmark project.

---
+++ bench/NetQD.Benchmark/LibraryBenchmarks.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.CsProj;
+
+namespace NetQD.Benchmark
+{
+	[Config(typeof(Config))]
+	public class LibraryBenchmarks
+	{
+        public class Config : ManualConfig
+        {
+            public Config()
+            {
+                var baseJob = Job.MediumRun.With(CsProjCoreToolchain.Current.Value).WithLaunchCount(1).WithIterationCount(10);
+                Add(baseJob);
+            }
+        }
+
+        [Benchmark]
+		public QdReal Add()
+		{
+			QdReal a = 1;
+			QdReal sum = 0;
+			for (int i = 0; i < 1024; i++)
+			{
+				sum += a;
+			}
+
+			return sum;
+		}
+	}
+}
